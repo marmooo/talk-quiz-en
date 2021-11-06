a5cc1e23cef7b3b99c6847d7c2cebe1bf94ace7d
marmooo <marmooo@users.noreply.github.com>
2021-11-06 11:51:28 +0900

    improve TTS support alert

--- src/index.js
+++ src/index.js
@@ -97,10 +97,17 @@ function loadVoices() {
     if (voices.length !== 0) {
       resolve(voices);
     } else {
+      let supported = false;
       speechSynthesis.addEventListener("voiceschanged", function () {
+        supported = true;
         voices = speechSynthesis.getVoices();
         resolve(voices);
       });
+      setTimeout(() => {
+        if (!supported) {
+          document.getElementById("noTTS").classList.remove("d-none");
+        }
+      }, 1000);
     }
   });
   allVoicesObtained.then((voices) => {
@@ -187,7 +194,7 @@ document.getElementById("cse-search-box-form-id").onsubmit = searchByGoogle;
 
 function setVoiceInput() {
   if (!("webkitSpeechRecognition" in window)) {
-    document.getElementById("nosupport").classList.remove("d-none");
+    document.getElementById("noSTT").classList.remove("d-none");
   } else {
     const voiceInput = new webkitSpeechRecognition();
     voiceInput.lang = "en-US";
--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2021-11-04 00:15';
+var CACHE_NAME = '2021-11-06 11:50';
 var urlsToCache = [
   "/talk-quiz-en/",
   "/talk-quiz-en/0.lst",
