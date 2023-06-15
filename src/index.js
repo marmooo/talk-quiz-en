7d251ad76bcc9ec5f0943120fd4e950bf5a498d2
marmooo <marmooo@users.noreply.github.com>
2023-06-16 01:55:46 +0900

    fix voice

--- src/index.js
+++ src/index.js
@@ -23,10 +23,6 @@ function loadConfig() {
   if (localStorage.getItem("darkMode") == 1) {
     document.documentElement.dataset.theme = "dark";
   }
-  if (localStorage.getItem("voice") == 0) {
-    document.getElementById("voiceOn").classList.add("d-none");
-    document.getElementById("voiceOff").classList.remove("d-none");
-  }
 }
 
 function toggleDarkMode() {
