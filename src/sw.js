b8c00a4bb82a9a6d07fee7806ff46c923964eb4f
marmooo <marmooo@users.noreply.github.com>
2026-01-17 01:18:04 +0900

    cleanup

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const cacheName = "2025-11-21 00:00";
+const cacheName = "2026-01-17 00:00";
 const urlsToCache = [
   "/talk-quiz-en/data/0.tsv",
   "/talk-quiz-en/data/1.tsv",
@@ -20,7 +20,7 @@ async function preCache() {
   const cache = await caches.open(cacheName);
   await Promise.all(
     urlsToCache.map((url) =>
-      cache.add(url).catch((e) => console.warn("Failed to cache", url, e))
+      cache.add(url).catch((err) => console.warn("Failed to cache", url, err))
     ),
   );
   self.skipWaiting();
